c43cf5cfcc2a447059ad0accf7846c756ba53a26
Andrew Herrington <andrew.the.techie@gmail.com>
2021-05-29 20:39:56 -0500

    adds /learning-git as a route and a template

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,8 @@ from app import app
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/learning-git')
+def learning_git():
+    return render_template('git.html')
+
